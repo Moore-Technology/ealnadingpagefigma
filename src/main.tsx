0a786cd34f9d47611f7eb93f2cc427c7e49ec88b
cmoore-AISA <ccmoor3@gmail.com>
2026-02-26 03:59:11 +0000

    Add back domain prop to ClerkProvider for satellite sync - DNS now verified

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@
   }
 
 createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} domain="eacoachpro.com">
     <App />
   </ClerkProvider>
 );
